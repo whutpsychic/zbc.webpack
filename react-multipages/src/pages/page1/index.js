acfcb6010052245be13f5070bf11508e640bb292
b90_100007129645_zbc <b90_100007129645_zbc@git.cloud.tencent.com>
2019-10-31 18:15:41 +0800

    。。。

---
+++ react-multipages/src/pages/page1/index.js
@@ -0,0 +1,15 @@
+﻿import React from "react";
+import ReactDOM from "react-dom";
+
+class Default extends React.Component {
+	componentDidMount() {
+		console.log(11111122233333);
+	}
+
+	render() {
+		return <h3>zbc</h3>;
+	}
+}
+
+ReactDOM.render(<Default />, document.getElementById("root"));
+
---
+++ react-multipages/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+
+module.exports = {
+	mode: "production",
+	entry: ["./src/pages/page1/index.js"],
+	output: {
+		filename: "bundle.js",
+		path: path.resolve(__dirname, "dist")
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: "babel-loader"
+			}
+		]
+	}
+};
